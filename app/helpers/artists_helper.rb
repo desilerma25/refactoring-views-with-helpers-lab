a3893654aada2a004dd934c6b8f5e3755271e28e
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-10 22:27:50 -0600

    commit tree 810e307e926c2836be0bd8ab91bfc398ee84df6e

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        if song.artist && song.artist.name !="" # if song artist and song artist name do NOT eq nil
+            link_to song.artist.name, artist_path(song.artist) # link song artist name 
+        else #otherwise
+            link_to "Add Artist", edit_song_path(song) # link back to edit artist
+        end
+    end
 end
